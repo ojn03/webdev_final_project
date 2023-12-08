61dadd273481c3e443346288eb46de3ac57af30c
Aidan Rosenberg <aidan@rosenbergs.org>
2023-12-07 22:14:12 -0500

    added css and setup tailwind

--- src/navigation/nav-side.js
+++ src/navigation/nav-side.js
@@ -1,10 +1,27 @@
-import { React} from "react";
+import { React } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/global-styles.css";
+import { MdHomeFilled } from "react-icons/md";
+import { FiSearch } from "react-icons/fi";
+import { FaUserCircle } from "react-icons/fa";
+import './nav.css';
 
 function NavBarSide() {
-    
-  return (
-    <div></div>
-  );
+	const links = ["home", "search", "profile"];
+	const icons = [<MdHomeFilled />, <FiSearch />, <FaUserCircle />];
+	const { pathname } = useLocation();
+
+	return (
+		<div className="flex flex-col p-2 justify-start items-center wd-nav-bar">
+			{links.map((link, index) => (
+				<Link
+					key={index}
+					to={`/${link}`}
+					className={`wd-nav-icon text-3xl m-2 ${pathname.includes(link) && "active"}`}>
+					{icons[index]}
+				</Link>
+			))}
+		</div>
+	);
 }
 export default NavBarSide;
