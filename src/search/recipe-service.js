96a5050fff6adcb550152a633f6e5d500a44cd72
Andrew Yang <yang.and@northeastern.edu>
2023-12-07 22:18:06 -0500

    working recipes

---
+++ src/search/recipe-service.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+const KEY = process.env.REACT_APP_RECIPE_API_KEY;
+const RECIPE_API = "https://api.edamam.com/api/recipes/v2";
+const NAPSTER_IMAGE_URL = "https://api.napster.com/imageserver/v2";
+const APP_ID = '83c6cb3e';
+export const albumImageUrl = (album) =>
+    `${NAPSTER_IMAGE_URL}/albums/${album.id}
+/images/300x300.jpg`;
+
+export const fetchRecipe = async (query) => {
+
+    const params = {
+        q: query,
+        app_key: KEY,
+        type: 'public',
+        app_id: APP_ID,
+    };
+
+    const apiUrl = `${RECIPE_API}?${new URLSearchParams(params)}`;
+    console.log(apiUrl)
+    console.log('hell')
+    try {
+        const response = await axios.get(apiUrl);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+        throw error; // Re-throw the error to propagate it further if needed
+    }
+};
+
+
+// export const fullTextSearch = async (text) => {
+//     const response = await axios.get(
+//         `${NAPSTER_API}/search/verbose?query=${text}&apikey=${KEY}`
+//     );
+//     return response.data;
+// };
+
+// export const albumDetails = async (albumId) => {
+//     const response = await axios.get(
+//         `${NAPSTER_API}/albums/${albumId}?apikey=${KEY}`
+//     );
+//     return response.data;
+// };
+
+// export const albumTracks = async (trackId) => {
+//     const response = await axios.get(
+//         `${NAPSTER_API}/albums/${trackId}?apikey=${KEY}`
+//     );
+//     return response.data;
+// };
--- src/search/searchpage.js
+++ src/search/searchpage.js
@@ -1,10 +1,45 @@
-import { React} from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/global-styles.css";
+import { fetchRecipe } from "./recipe-service";
 
 function Search() {
-    
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleFetchRecipe = async () => {
+    try {
+      const result = await fetchRecipe(searchTerm); // Assuming you have a fetchRecipe function
+      console.log(result)
+      setData(result || []);
+    } catch (error) {
+      console.error("Error fetching recipe:", error);
+    }
+  }
+
   return (
-    <div></div>
+    <div>
+      <input
+        type="text"
+        placeholder="Type here..."
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleFetchRecipe}>Fetch Recipe</button>
+      {data.hits && data.hits.map((hit, index) => (
+        <div key={index}>
+          <img
+            src={hit.recipe.image}
+            alt={hit.recipe.label}
+            style={{ maxWidth: '100%', maxHeight: '200px' }}
+          />
+          {JSON.stringify(hit.recipe.label)}
+        </div>
+      ))}    </div>
   );
 }
+
 export default Search;
