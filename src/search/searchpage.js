02a87a7f8f076a9d00fbdeb90b05e2ccc177e5ba
Andrew Yang <yang.and@northeastern.edu>
2023-12-07 22:54:09 -0500

    bootstrap

--- src/search/searchpage.js
+++ src/search/searchpage.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "../styles/global-styles.css";
 import { fetchRecipe } from "./recipe-service";
+import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom'; // Make sure to have react-router-dom installed
+
 
 function Search() {
   const [data, setData] = useState([]);
@@ -27,19 +30,31 @@ function Search() {
         placeholder="Type here..."
         value={searchTerm}
         onChange={handleInputChange}
+        className="form-control mb-3"
       />
-      <button onClick={handleFetchRecipe}>Fetch Recipe</button>
-      {data.hits && data.hits.map((hit, index) => (
-        <div key={index}>
-          <img
-            src={hit.recipe.image}
-            alt={hit.recipe.label}
-            style={{ maxWidth: '100%', maxHeight: '200px' }}
-          />
-          {JSON.stringify(hit.recipe.label)}
-        </div>
-      ))}    </div>
-  );
+      <Button onClick={handleFetchRecipe} variant="primary" className="mb-3">
+        Fetch Recipe
+      </Button>
+      <div className="row">
+        {data.hits &&
+          data.hits.map((hit, index) => (
+            <div key={index} className="col-md-4 mb-3">
+              <Link
+                to={`/recipe/${hit.recipe.uri}`}
+                style={{ textDecoration: 'none', color: 'inherit' }}
+              >
+                <Card>
+                  <Card.Img variant="top" src={hit.recipe.image} alt={hit.recipe.label} />
+                  <Card.Body>
+                    <Card.Title>{hit.recipe.label}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </div>
+          ))}
+      </div>
+    </div>
+  )
 }
 
 export default Search;
