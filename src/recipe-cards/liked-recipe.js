0f4f3253dadd7fc04cdd41c32e7e2ba840d362b5
Aidan Rosenberg <aidan@rosenbergs.org>
2023-12-13 18:33:51 -0500

    connecting client to recipe page and cards

--- src/recipe-cards/liked-recipe.js
+++ src/recipe-cards/liked-recipe.js
@@ -3,61 +3,74 @@ import { Link } from "react-router-dom";
 import "../styles/global-styles.css";
 import "./recipe-cards.css";
 import {
-	FaRegCircleCheck,
-	FaCircleCheck,
 	FaHeart,
 	FaRegHeart,
 	FaRegComment,
-	FaComment,
 } from "react-icons/fa6";
+import * as client from "../client.js";
+import { fetchRecipeById } from "../search/recipe-service";
 
-function LikedRecipeCard({recipeId, likedDate}) {
-	let dummyRecipeTitle = "Creamy Mashed Potatoes With Butter and Herbs";
-	let dummyImgUrl =
-		"https://www.allrecipes.com/thmb/JMkfc1WFQiiKmTzvz98IXhlOMsg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/24771-basic-mashed-potatoes-mfs321-158e1626bfeb48daadb4f25d737ffee91-912ba4ee2c3c40a1837a5690971cc554.jpg";
-	let dummyLikedDate = "1/12/2023";
-	let dummyLikes = 14;
-	let dummyEndorsements = 3;
-	let dummyReviews = 0;
+function LikedRecipeCard({likeId}) {
+	
 
-	let link = 1;
+	const [like, setLike] = useState(null);
+	const [recipe, setRecipe] = useState(null);
+	const [reviews, setReviews] = useState(null);
+	const [likes, setLikes] = useState(null);
+
+	const fetchLike = async () => {
+		const like = await client.fetchLikeById(likeId);
+		setLike(like);
+	}; 
+
+	const fetchLikes = async () => {
+		const likes = await client.findLikes(recipe.id);
+		setLikes(likes);
+	};
+
+	const fetchReviews = async () => {
+		const reviews = await client.findComments(recipe.id);
+		setReviews(reviews);
+	};
+ 
+	useEffect(() => {
+		fetchLike();
+		fetchRecipeById(like.recipeId).then((recipe) => {
+			setRecipe(recipe);
+		});
+		fetchLikes();
+		fetchReviews();
+	}, []);
 
 	return (
-		<Link to={`/recipe/${link}`}>
+		<Link to={`/recipe/${recipe.id}`}>
 			<div className="container wd-card my-0 p-0">
-				{/* <div className="m-0 p-0 wd-img-container" style={{height:elemHeight}}>
-                <img src={dummyImgUrl} alt="test"></img>
-            </div> */}
-
 				<div
 					id="wd-card-title"
 					className="wd-card-title flex align-left flex-auto flex-col p-6 content-end justify-between">
 					<div>
 						<span className="text-xs italic text-stone-500 wd-liked-on">
-							<FaHeart /> <p>You liked on {dummyLikedDate}</p>
+							<FaHeart /> <p>You liked on {like.created}</p>
 						</span>
 						<p className="wd-recipe-title py-1">
-							{dummyRecipeTitle}
+							{recipe.label}
 						</p>
 					</div>
 
 					<div className="bottom-0 max-h-32 flex align-left flex-auto flex-col bg-stone-200 p-2 px-3 rounded-md justify-center text-stone-600">
 						<span className="wd-inline-stats">
-							<FaRegHeart /> <p>{dummyLikes} likes</p>
+							<FaRegHeart /> <p>{likes ? likes.length : 0} likes</p>
 						</span>
+						
 						<span className="wd-inline-stats">
-							<FaRegCircleCheck />{" "}
-							<p>{dummyEndorsements} endorsements</p>
-						</span>
-						<span className="wd-inline-stats">
-							<FaRegComment /> <p>{dummyReviews} reviews</p>
+							<FaRegComment /> <p>{reviews ? reviews.length : 0} reviews</p>
 						</span>
 					</div>
 				</div>
 				<div
 					className="m-0 p-0 wd-img-container"
 					>
-					<img  src={dummyImgUrl} alt="test"></img>
+					<img  src={recipe.image} alt={recipe.label}></img>
 				</div>
 			</div>
 		</Link>
