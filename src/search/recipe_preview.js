9b80a70e863f5136767f80f0dc6d26a41794e25b
Andrew Yang <yang.and@northeastern.edu>
2023-12-14 13:26:38 -0500

    profile working

--- src/search/recipe_preview.js
+++ src/search/recipe_preview.js
@@ -6,28 +6,32 @@ import "./search-styles.css";
 
 const REGEX_TO_MATCH = /#recipe_([^\/]+)$/;
 
+export const getRecipeIdFromUri = (uri) => {
+	return uri.match(REGEX_TO_MATCH)[1];
+}
+
 function RecipePreview({ apiResult }) {
-// 	const match = REGEX_TO_MATCH.exec(apiResult.recipe.uri);
-const match = apiResult.recipe.uri.match(REGEX_TO_MATCH)[1];
+	// 	const match = REGEX_TO_MATCH.exec(apiResult.recipe.uri);
+	const match = apiResult.recipe.uri.match(REGEX_TO_MATCH)[1];
 
 	return (
 		<div className="col-md-4 mb-3">
 			<Link
 				to={`/recipe/${match}`}
 				style={{ textDecoration: "none", color: "inherit" }}>
-				
-					<div className="wd-search-card">
-						<img
-							variant="top"
-							src={apiResult.recipe.image}
-							alt={apiResult.recipe.label}
-                            className="wd-search-img"
-						/>
-						<div className="p-2">
-							<h2 className="wd-sub-title text-stone-600">{apiResult.recipe.label}</h2>
-						</div>
+
+				<div className="wd-search-card">
+					<img
+						variant="top"
+						src={apiResult.recipe.image}
+						alt={apiResult.recipe.label}
+						className="wd-search-img"
+					/>
+					<div className="p-2">
+						<h2 className="wd-sub-title text-stone-600">{apiResult.recipe.label}</h2>
 					</div>
-			
+				</div>
+
 			</Link>
 		</div>
 	);
